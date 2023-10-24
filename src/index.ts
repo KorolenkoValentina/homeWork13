efa8f7d7351efbf1472cb34e2b8d81fdd6ae733f
Valentina <korolenkovalya0704@gmail.com>
2023-10-24 19:43:43 +0300

    add

--- src/index.ts
+++ src/index.ts
@@ -15,66 +15,64 @@
 
 
 class Note {
-  private id: number;
-  private title: string;
-  private content: string;
-  private createdDate: Date;
-  private isConfirmed: boolean;
+
+  private _createdDate: Date;
+  private _isConfirmed: boolean;
   private modifiedDate: Date;
-  private type: 'default' | 'requiresConfirmation';
 
-
-  constructor(id: number, title: string, content: string, type: 'default' | 'requiresConfirmation') {
-    this.id = id;
-    this.title = title;
-    this.content = content;
-    this.createdDate = new Date();
-    this.isConfirmed = false;
-    this.modifiedDate = this.createdDate;
-    this.type = type;
+  constructor( 
+    private id: number,
+    private _title: string,
+    private _content: string,
+    private type: 'default' | 'requiresConfirmation' = 'default'
+    ){
+    this._createdDate = new Date();
+    this._isConfirmed = false;
+    this.modifiedDate = this._createdDate;
+    
   }
 
-  getId() {
+  get getId(): number {
     return this.id;
   }
 
-  getTitle() {
-    return this.title;
+  get title(): string {
+    return this._title;
   }
 
-  setTitle(newTitle: string) {
-    this.title = newTitle;
+  set title(newTitle: string) {
+    this._title = newTitle;
   }
 
-  getContent() {
-    return this.content;
+  get content(): string {
+    return this._content;
   }
 
-  setContent(newContent: string) {
-    this.content = newContent;
+  set content(newContent: string) {
+    this._content = newContent;
   }
 
-  getCreatedDate() {
-    return this.createdDate;
+  get createdDate(): Date {
+    return this._createdDate;
   }
 
-  getModifiedDate() {
+  get isConfirmed(): boolean {
+    return this._isConfirmed;
+  }
+
+  set isConfirmed(isConfirmed: boolean) {
+    this._isConfirmed = isConfirmed;
+  }
+
+  getModifiedDate = (): Date => {
     return this.modifiedDate;
   }
-
-  updateModifiedDate() {
+  
+  updateModifiedDate = (): void => {
     this.modifiedDate = new Date();
   }
-
-  getIsConfirmed() {
-    return this.isConfirmed;
-  }
-
-  setIsConfirmed(isConfirmed: boolean) {
-    this.isConfirmed = isConfirmed;
-  }
-
-  confirmEdit() {
+  
+  confirmEdit = (): void => {
     this.isConfirmed = true;
   }
 }
@@ -99,7 +97,7 @@ class TodoList {
 
   // Метод для видалення нотатки за ідентифікатором
   deleteNote(id: number) {
-    const noteIndex = this.notes.findIndex((note) => note.getId() === id);
+    const noteIndex = this.notes.findIndex((note) => note.getId === id);
     if (noteIndex !== -1) {
       this.notes.splice(noteIndex, 1);
     }
@@ -107,10 +105,10 @@ class TodoList {
 
   // Метод для редагування нотатки за ідентифікатором
   editNote(id: number, newTitle: string, newContent: string) {
-    const note = this.notes.find((note) => note.getId() === id);
+    const note = this.notes.find((note) => note.getId === id);
     if (note) {
-      note.setTitle(newTitle);
-      note.setContent(newContent);
+      note.title = newTitle;
+      note.content = newContent;
       note.updateModifiedDate();
     }
   }
@@ -118,48 +116,49 @@ class TodoList {
 
 
   // Метод для отримання нотатки за ідентифікатором
-  getNoteById(id: number) {
-    return this.notes.find((note) => note.getId() === id);
+  getNoteById(id: number): Note | undefined  {
+    return this.notes.find((note) => note.getId === id);
   }
 
 
 
   // Метод для отримання списку всіх нотаток
-  getNoteList() {
+  getNoteList(): Note[]{
     return this.notes;
   }
 
 
    // Метод для підтвердження редагування нотатки
   confirmEdit(id: number) {
-    const note = this.notes.find((note) => note.getId() === id);
+    const note = this.notes.find((note) => note.getId === id);
     if (note) {
       note.confirmEdit();
     }
   }
 
   // Метод для підрахунку загальної кількості нотаток
-  getTotalNotes() {
+  getTotalNotes(): number {
     return this.notes.length;
   }
 
   // Метод для підрахунку кількості невиконаних нотаток
-  getUnconfirmedNotesCount() {
-    return this.notes.filter((note) => !note.getIsConfirmed()).length;
+  getUnconfirmedNotesCount(): number {
+    return this.notes.filter((note) => !note.isConfirmed).length;
   }
 
   // Метод для позначення нотатки, як виконаної, за ідентифікатором
-  markNoteAsDone(id: number) {
-    const note = this.notes.find((note) => note.getId() === id);
+  markNoteAsDone(id: number): void {
+    const note = this.notes.find((note) => note.getId === id);
     if (note) {
-      note.setIsConfirmed(true);
+      note.isConfirmed = true;
     }
   }
+
   // Метод для пошуку нотаток за ім'ям або змістом
-  searchNotes(query: string) {
+  searchNotes(query: string): Note[] {
     return this.notes.filter(
       (note) =>
-        note.getTitle().includes(query) || note.getContent().includes(query)
+        note.title.includes(query) || note.content.includes(query)
     );
   }
 
@@ -168,17 +167,19 @@ class TodoList {
   sortNotes(by: 'status' | 'createdDate'): Note[] {
     if (by === 'status') {
       return this.notes.sort((a, b) => {
-        if (a.getIsConfirmed() === b.getIsConfirmed()) {
+        if (a.isConfirmed === b.isConfirmed) {
           return 0;
         }
-        return a.getIsConfirmed() ? 1 : -1;
+        return a.isConfirmed ? -1 : 1;
       });
     } else if (by === 'createdDate') {
-      return this.notes.sort((a, b) => a.getCreatedDate().getTime() - b.getCreatedDate().getTime());
+      return this.notes.slice().sort((a, b) => a.createdDate.getTime() - b.createdDate.getTime());
     }
-   return [];
+    return [];
   }
 }
+
+
 
 const todoList = new TodoList();
 todoList.addNote("Завдання 1", "Сходити в супермаркет" )
@@ -187,45 +188,39 @@ todoList.addNote("Завдання 3", "Приготувати обід")
 todoList.addNote("Завдання 4", "Зробити уроки з дитиною")
 console.log(todoList.getNoteList());
 
-// todoList.deleteNote(1); 
-// console.log(todoList.getNoteList());
+todoList.deleteNote(1); 
+console.log(todoList.getNoteList());
 
-// todoList.editNote(3,"Завдання 3", "Приготувати сніданок");
-// console.log(todoList.getNoteList());
+todoList.editNote(3,"Завдання 3", "Приготувати сніданок");
+console.log(todoList.getNoteList());
 
-// const note = todoList.getNoteById(2);
-// console.log(note?.getTitle()); 
-// console.log(note?.getContent()); 
-
-
-// const allNotes = todoList.getNoteList();
-// console.log(allNotes);
+const note = todoList.getNoteById(2);
+console.log(note?.title);
+console.log(note?.content);
 
 
-// const notes = todoList.getNoteById(2);
+const allNotes = todoList.getNoteList();
+console.log(allNotes);
 
-// if (notes) {
-//   console.log(notes.getIsConfirmed());
-// } else {
-//   console.log("Нотатка з ідентифікатором 2 не знайдена.");
-// }
 
-// const totalNotes = todoList.getTotalNotes();
-// console.log(`Загальна кількість нотаток: ${totalNotes}`);
+todoList.confirmEdit(2); 
 
-// const unconfirmedNotes = todoList.getUnconfirmedNotesCount();
-// console.log(`Кількість невиконаних нотаток: ${unconfirmedNotes}`);
+const totalNotes = todoList.getTotalNotes();
+console.log(`Загальна кількість нотаток: ${totalNotes}`);
 
-// todoList.markNoteAsDone(1); 
+const unconfirmedNotes = todoList.getUnconfirmedNotesCount();
+console.log(`Кількість невиконаних нотаток: ${unconfirmedNotes}`);
 
-// const searchResults = todoList.searchNotes("обід");
-// console.log(searchResults); 
+todoList.markNoteAsDone(1); 
 
-// const sortedByStatus = todoList.sortNotes("status"); 
-// console.log(sortedByStatus);
+const searchResults = todoList.searchNotes("обід");
+console.log(searchResults); 
 
-// const sortedByCreatedDate = todoList.sortNotes("createdDate"); 
-// console.log(sortedByCreatedDate);
+const sortedByStatus = todoList.sortNotes("status"); 
+console.log(sortedByStatus);
+
+const sortedByCreatedDate = todoList.sortNotes("createdDate"); 
+console.log(sortedByCreatedDate);
 
 
 
